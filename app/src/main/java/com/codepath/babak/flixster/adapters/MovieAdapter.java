5b9dedc185fd7d62fdb2adabe7b28e28d16a19ea
bobby-shaj <57640405+bobby-shaj@users.noreply.github.com>
2021-09-10 21:42:49 -0700

    9:42

--- app/src/main/java/com/codepath/babak/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/babak/flixster/adapters/MovieAdapter.java
@@ -2,9 +2,12 @@ package com.codepath.babak.flixster.adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
